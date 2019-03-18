653e4ff7debfa5caba0a14b0b71714feca59abf7
Nadezhda Yakunina <nadynady98@mail.ru>
2019-03-18 22:48:28 +0300

    part 1: two buttons only (add and exit)

--- example03.py
+++ example03.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python3
-'''
-Пример для первой лекции про TkInter
-
-Закрытие окошка в постинтерактивном режиме
-'''
 
 from tkinter import *
-
-def dump(*args):
-    print("DUMP:",args)
 
 TKroot = Tk()
 TKroot.title("Hello")
@@ -21,13 +13,10 @@ root.columnconfigure(1, weight=2)
 root.rowconfigure(0, weight=10)
 root.rowconfigure(1, weight=1)
 
-Butt = Button(root, text="Butt ON")
-Butt.bind('<Button-1>', dump)
+Butt = Button(root, text="Add")
 Butt.grid(row=0, column=0, sticky=E+W+S+N)
-Exit = Button(root, text="Quit!", command=root.quit)
+Exit = Button(root, text="Exit", command=root.quit)
 Exit.grid(row=0, column=1, sticky=E+W+S+N)
-Txt = Label(root, text="This is a label", bg="PeachPuff")
-Txt.grid(row=1, column=0, columnspan=2, sticky=E+W+N)
 
 TKroot.mainloop()
 print("Done")
